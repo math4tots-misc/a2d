1d696abca361b8a8fbc0058382491d8d7783c34b
Kyumin Kim <math4tots@users.noreply.github.com>
2020-06-22 10:53:30 -0700

    Use OUT_DIR in build.rs
    Instead of generating files directly in src/

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ extern crate shaderc;
 
 use shaderc::Compiler;
 use shaderc::ShaderKind;
+use std::env;
 use std::fs;
 use std::path::MAIN_SEPARATOR;
 
@@ -11,19 +12,23 @@ fn main() {
 
 fn build_shaders() {
     let mut compiler = Compiler::new().unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:warning={}", out_dir);
     build_shader(
         &mut compiler,
+        &format!("{}/shader.vert.spirv", out_dir),
         &pt("src/shaders/shader.vert"),
         ShaderKind::Vertex,
     );
     build_shader(
         &mut compiler,
+        &format!("{}/shader.frag.spirv", out_dir),
         &pt("src/shaders/shader.frag"),
         ShaderKind::Fragment,
     );
 }
 
-fn build_shader(compiler: &mut Compiler, path: &str, kind: ShaderKind) {
+fn build_shader(compiler: &mut Compiler, out_path: &str, path: &str, kind: ShaderKind) {
     println!("cargo:rerun-if-changed={}", path);
     let src = fs::read_to_string(path).unwrap();
     let name = path.rsplit(MAIN_SEPARATOR).next().unwrap_or(path);
@@ -31,7 +36,7 @@ fn build_shader(compiler: &mut Compiler, path: &str, kind: ShaderKind) {
         .compile_into_spirv(&src, kind, name, "main", None)
         .unwrap();
     let data = spirv.as_binary_u8();
-    fs::write(format!("{}{}", path, ".spirv"), &data).unwrap();
+    fs::write(out_path, &data).unwrap();
 
     // For debugging if needed
     // println!("cargo:warning={}", format!("{}{}", path, ".spirv"));
--- src/shaders/mod.rs
+++ src/shaders/mod.rs
@@ -1,2 +1,22 @@
-pub const VERT: &[u8] = include_bytes!("shader.vert.spirv");
-pub const FRAG: &[u8] = include_bytes!("shader.frag.spirv");
+#[cfg(target_os = "windows")]
+macro_rules! sep {
+    () => {
+        "\\"
+    };
+}
+
+#[cfg(not(target_os = "windows"))]
+macro_rules! sep {
+    () => {
+        "/"
+    };
+}
+
+macro_rules! get_bytes {
+    ($filename:tt) => {
+        include_bytes!(concat!(env!("OUT_DIR"), sep!(), $filename))
+    };
+}
+
+pub const VERT: &[u8] = get_bytes!("shader.vert.spirv");
+pub const FRAG: &[u8] = get_bytes!("shader.frag.spirv");
